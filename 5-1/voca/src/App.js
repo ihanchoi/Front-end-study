29ad0a04e8c865c1776a66cfad10082f24216c5a
ihanchoi <ihanchoi.dev@gmail.com>
2023-05-22 15:27:24 +0900

    create css module

--- 5-1/voca/src/App.js
+++ 5-1/voca/src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import Hello from './component/Hello';
 import Welcome from './component/Welcome';
 import World from './component/World';
+import styles from "./App.module.css";
 
 function App() {
   return <div className="App">
       <Hello />
-      <Welcome />
-      <World />
+      <div className={styles.box}>App</div>
     </div>;
 }
 export default App;
