7269b115881e2dbd22b8d2404cb667f9d11ebb15
ihanchoi <ihanchoi.dev@gmail.com>
2023-05-22 10:57:21 +0900

    create component

--- 5-1/voca/src/App.js
+++ 5-1/voca/src/App.js
@@ -1,25 +1,13 @@
-// import logo from './logo.svg';
 import './App.css';
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
+import World from './component/World';
 
 function App() {
-  const name = "Han";
-  const google = {
-    name: "google",
-    url: "https://google.com",
-  };
-  return (
-    <div className="App">
-      <h1 
-      style={{
-        color: "#0080ff",
-        backgroundColor: "lightblue",
-    }}
-    >
-      Hello, {name}. <p>{2+3}</p>
-      </h1>
-      <a href={google.url}>{google.name}</a>
-    </div>
-  );
+  return <div className="App">
+      <Hello />
+      <Welcome />
+      <World />
+    </div>;
 }
-
 export default App;
